9a75f7ebef3492439fceb60c89a4f1e7e395ff01
Juanita Jimenez <juanitatolosa94@gmail.com>
2025-09-01 17:49:06 -0500

    agrege manipulacion de estilos y clases de javaScript

--- js/modificar-estilos.js
+++ js/modificar-estilos.js
@@ -5,6 +5,7 @@ caja1.style.backgroundColor="#dca7e9ff";
 caja1.style.color="#222122";
 caja1.style.textAlign="center";
 
+//cambiar estilos a elemtentos multiples teniendo en cuenta el contenedor padre 
 const cajaEstilos=document.querySelectorAll("#estilos .caja");
 
 cajaEstilos.forEach((cajaEstilos, index) => {
@@ -12,4 +13,9 @@ cajaEstilos.style.border="2px solid blue";
 
 }
 
-)
+);
+
+//manipulacion de clase
+ caja1.classList.add("active");
+
+
