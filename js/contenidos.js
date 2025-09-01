755ac87b5c65791f9ab9d2a1e016a06b1a36b97a
Juanita Jimenez <juanitatolosa94@gmail.com>
2025-09-01 17:00:52 -0500

    agregue manipulacón de contenidos multiples

--- js/contenidos.js
+++ js/contenidos.js
@@ -1,5 +1,21 @@
+// antes de las variablesd cambiar los elemntos unicos 
 const cajaUnica=document.getElementById("caja-unica");
+const titulo=document.querySelector("#titulo2");
 
-cajaUnica.innerText="Cambio en el texto numero #1 con inner.Text" 
-cajaUnica.innerHTML="Cambio en el texto #2 con innerHtml"
-cajaUnica.textContent="Cambio en el texto #2 con textContent"
+
+cajaUnica.innerText="Cambio en el texto numero #1 con inner.Text";
+cajaUnica.innerHTML="Cambio en el texto #2 con innerHtml";
+cajaUnica.textContent="Cambio en el texto #2 con textContent";
+titulo.innerText="Manipulación de estilos css y clases";
+
+//cambiar contenidos a elemntos multiples 
+const cajas=document.querySelectorAll(".caja-contenidos");
+
+cajas.forEach((caja, index) => {
+    caja.textContent="Caja multiple numero" + index;
+    caja.style.color="red";
+    caja.style.fontSize="20px";
+    caja.style.textAlign="right";
+}
+
+);
