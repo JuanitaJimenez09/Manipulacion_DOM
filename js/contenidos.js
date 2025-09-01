670ddd243d1bdfd7cdf5124d30a016ea5f8e2136
Juanita Jimenez <juanitatolosa94@gmail.com>
2025-09-01 15:45:44 -0500

    agrege una funcionalidad de cambiar contenidos

---
+++ js/contenidos.js
@@ -0,0 +1,5 @@
+const cajaUnica=document.getElementById("caja-unica");
+
+cajaUnica.innerText="Cambio en el texto numero #1 con inner.Text" 
+cajaUnica.innerHTML="Cambio en el texto #2 con innerHtml"
+cajaUnica.textContent="Cambio en el texto #2 con textContent"
