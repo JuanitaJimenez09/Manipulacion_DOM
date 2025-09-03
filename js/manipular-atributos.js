0290c19c21c825ebd807f54f5e90c2391d8ea024
Juanita Jimenez <juanitatolosa94@gmail.com>
2025-09-03 14:48:12 -0500

    agregue el logo de facebook

---
+++ js/manipular-atributos.js
@@ -0,0 +1,5 @@
+const facebook = document.getElementById("btn-facebook");
+facebook.innerHTML ='<img src="img/facebook.png" alt="Facebook">';
+
+const manipular = document.querySelector('#imagen-unica');
+manipular.setAttribute('alt', 'texto alternativo modificado con javascript');
